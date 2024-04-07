7e286cd0bca75192bb20d6c54e6a7d789a24bad7
Utkarsh-Web-2023 <utkarshweb2023@google.com>
2024-04-07 15:22:28 +0530

    stable release of 0.5v

--- src/Components/main.jsx
+++ src/Components/main.jsx
@@ -86,7 +86,7 @@ function Main(props) {
 
     useEffect(() => {
         // Function to handle keydown events
-        const handleKeyUp = (event) => {
+        const handleKeyDown = (event) => {
             // Check if any key is pressed
             if (!isTyping) {
                 setIsTyping(true); // Set typing status to true when user starts typing
@@ -95,7 +95,7 @@ function Main(props) {
         };
 
         // Function to handle keyup events
-        const handleKeyDown = () => {
+        const handleKeyUp = () => {
             setIsTyping(false); // Set typing status to false when user stops typing
             stopEditSound(); // Stop edit sound when user stops typing
         };
