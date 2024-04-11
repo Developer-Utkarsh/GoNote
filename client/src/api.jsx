f85048446983f85157097b7fad966817acc30f4d
Utkarsh Web <128823414+Utkarsh-Web-2023@users.noreply.github.com>
2024-04-11 17:01:49 +0530

    Update api.jsx

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://go-note.vercel.app/api';
 const api = axios.create({
     baseURL: API_BASE_URL,
 });
